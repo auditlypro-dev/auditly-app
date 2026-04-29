791e7e55f8cf3484c882b1ecf6c648f3a7a634ae
auditlypro-dev <auditlypro@gmail.com>
2026-04-29 00:35:14 -0700

    create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Auditly Pro is live 🚀");
+});
+
+app.get("/auth/callback", (req, res) => {
+  res.send("Auth callback working ✔");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});

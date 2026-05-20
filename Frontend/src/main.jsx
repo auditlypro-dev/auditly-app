c82ff1301fc19c841ddd53e9cdbba86ad863c812
auditlypro-dev <auditlypro@gmail.com>
2026-05-20 01:36:13 -0700

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
-import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <AppProvider i18n={{}}>
     <App />
-  </React.StrictMode>
+  </AppProvider>
 );
